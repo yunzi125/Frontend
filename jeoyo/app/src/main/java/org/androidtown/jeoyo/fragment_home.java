7b2994581b5fdae4ae520aa7f8817022c9cb9adc
Go-Eun Park <dbtshfdl1@gmail.com>
2022-02-18 19:32:09 +0900

    레이아웃 수정본 합침

--- jeoyo/app/src/main/java/org/androidtown/jeoyo/fragment_home.java
+++ jeoyo/app/src/main/java/org/androidtown/jeoyo/fragment_home.java
@@ -1,5 +1,6 @@
 package org.androidtown.jeoyo;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -14,6 +15,19 @@ public class fragment_home extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        View gong_next = view.findViewById(R.id.gong_next);
+        View dae_next = view.findViewById(R.id.dae_next);
+        View people_next = view.findViewById(R.id.people_next);
+        View study_next = view.findViewById(R.id.study_next);
+
+        gong_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), activity_home_gong.class);
+                startActivity(intent);    //intent 에 명시된 액티비티로 이동
+            }
+        });
+    return view;
     }
 }
